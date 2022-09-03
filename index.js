47374bc3148a7d8e6d307aae7cce8cd4ed0194ac
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-09-03 21:29:42 +0300

    Lab attempted

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+// Array find method lab
+
+superbowlWin = (record) => {
+    let result = record.find( record => record.result === "W" )
+    return !!result ? result.year : undefined
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
+  
